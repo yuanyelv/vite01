da9a0cf81f1c80b73ec66db31929d4e2872c2eb1
yuanyelv <287142032@qq.com>
2022-05-24 17:19:21 +0800

    @

--- src/views/home/index-02.vue
+++ src/views/home/index-02.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: yuanyelv 287142032@qq.com
+ * @Date: 2022-05-21 00:17:13
+ * @LastEditors: yuanyelv 287142032@qq.com
+ * @LastEditTime: 2022-05-24 17:13:43
+ * @FilePath: \vite01\src\views\home\index-02.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <h1>首页</h1>
   <p>{{ $store.state.count }}</p>
@@ -7,7 +15,8 @@
 <script lang="ts" setup>
 // import { useStore } from 'vuex'
 import { useStore } from '../../store'
-
+import User from '@/api/user'
+console.log(User)
 const store = useStore()
 
 console.log(store.state.count)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/*
+ * @Author: yuanyelv 287142032@qq.com
+ * @Date: 2022-05-20 20:42:28
+ * @LastEditors: yuanyelv 287142032@qq.com
+ * @LastEditTime: 2022-05-24 17:13:20
+ * @FilePath: \vite01\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEr
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +18,10 @@ export default defineConfig({
     vue(),
     eslint(),
     vueJsx()
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
